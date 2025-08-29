fd40449ac128e75fa7b33f7466a106ed9cb170d3
Abraham Medina <abrahammdnlpz@gmail.com>
2025-08-29 03:35:18 -0600

    Feat: Se desarrolla lógica de negocio orientada a tickets

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -2,10 +2,12 @@
 import { createClient } from '@supabase/supabase-js';
 import { env } from '../config/env.js';
 
+// Si existen los elementos de conexión a Supabase, se crea el servicio
 export const supabase = env.supabase.url && env.supabase.serviceRoleKey
   ? createClient(env.supabase.url, env.supabase.serviceRoleKey)
   : null;
 
+// Función que permite cargar/generar imágenes de los eventos en supabase: Se usa para crear un ticket y generar un QR 
 export async function uploadPng(buffer, path) {
   if (!supabase) throw new Error('Supabase not configured');
   const bucket = env.supabase.bucket;
@@ -14,6 +16,7 @@ export async function uploadPng(buffer, path) {
     upsert: true
   });
   if (error) throw error;
+  // URL de la imagen almacenada
   const { data: pub } = supabase.storage.from(bucket).getPublicUrl(path);
   return pub.publicUrl;
 }
